d0e3e85cd9828ea7f8e24674996a8bae989ed6cb
muratkoz <mmrtkz@gmail.com>
2021-03-05 13:08:34 +0100

    CompanyStepDefs class created

---
+++ src/test/java/com/CRMLY/stepdefinitions/CompanyStepDefs.java
@@ -0,0 +1,66 @@
+package com.CRMLY.stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CompanyStepDefs {
+    @When("the user clicks on News button")
+    public void the_user_clicks_on_News_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should display News window")
+    public void the_user_should_display_News_window() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicks on Add News button")
+    public void the_user_clicks_on_Add_News_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should display Add News window")
+    public void the_user_should_display_Add_News_window() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should display Official Information window")
+    public void the_user_should_display_Official_Information_window() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should display Our Life window")
+    public void the_user_should_display_Our_Life_window() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should display About Company window")
+    public void the_user_should_display_About_Company_window() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should display Photo Gallery Page")
+    public void the_user_should_display_Photo_Gallery_Page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should display Video Player window")
+    public void the_user_should_display_Video_Player_window() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should display news under Business News\\(RSS) menu")
+    public void the_user_should_display_news_under_Business_News_RSS_menu() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
